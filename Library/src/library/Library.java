b095fac7d0f7518c629c981810d631727fbdda0d
Miroslav Liubomirov Metodiev <miroslav.l.metodiev>
2013-05-29 10:59:34 +0300

    10:59

--- Library/src/library/Library.java
+++ Library/src/library/Library.java
@@ -15,5 +15,7 @@ public class Library {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("TEST");
+        //catch 
     }
 }
