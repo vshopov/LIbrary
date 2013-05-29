a89d36b0a3dc466cbc5a0f18176fb0e40f2bb45e
Miroslav Liubomirov Metodiev <miroslav.l.metodiev>
2013-05-29 11:50:37 +0300

    Add exeptions on set arguments

--- Library/src/library/Book.java
+++ Library/src/library/Book.java
@@ -3,6 +3,8 @@
  * and open the template in the editor.
  */
 package library;
+
+import java.lang.Character;
 /**
  *
  * @author miroslav.metodiev
@@ -21,24 +23,39 @@ public class Book {
 //        this.date = Date;
 //        this.ISBN = ISBN;
 //    }
-    
-
-  
-    public void setAuthor(String Author){
-        this.author = Author;
+   
+    public void setAuthor(String Author)
+        throws IllegalArgumentException {
+         //isDigit(Author[0]);
+        if (Author == null ) {
+           throw new IllegalArgumentException("Invalid Author");
+        }
+           this.author = Author;
+     
     }
     
     public void setPrice(double Price){
+        if (Price ==0 ) {
+            throw new IllegalArgumentException("Invalid Price");
+            
+        }
+        
         this.price = Price;
     }
     
     public void setDate(String Date){
+        if (Date == null) {
+            throw new IllegalArgumentException("Invalid Date ");        
+        }
         this.date = Date;
     }
     
     public void setISBN(long ISBN){
+        if (ISBN == 0) {
+            throw new IllegalArgumentException("Ivalid ISBN it must be at leat 13 characers");
+            
+        }
         this.ISBN = ISBN;
     }
-    
-    
+
 }
