b4746188c50905bba2094336b3a45d95458b4267
Miroslav Liubomirov Metodiev <miroslav.l.metodiev>
2013-05-29 11:14:02 +0300

    Add Book class

---
+++ Library/src/library/Book.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+/**
+ *
+ * @author miroslav.metodiev
+ */
+public class Book {
+    
+    private String author;
+    private double price;
+    private String date;
+    private double ISBN;
+    
+    public Book (String Author, double Price, String Date, double ISBN){
+    
+        this.author = Author;
+        this.price = Price;
+        this.date = Date;
+        this.ISBN = ISBN;
+    }
+    
+    
+  
+}
--- Library/src/library/Library.java
+++ Library/src/library/Library.java
@@ -14,7 +14,7 @@ public class Library {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
        TestPrintClass print = new TestPrintClass();
        print.PrintMethod();
        
