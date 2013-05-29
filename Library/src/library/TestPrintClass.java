4633b58f090dd20344866c2aad28553453a6aa84
Miroslav Liubomirov Metodiev <miroslav.l.metodiev>
2013-05-29 11:02:28 +0300

    11:02

---
+++ Library/src/library/TestPrintClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author miroslav.metodiev
+ */
+public class TestPrintClass {
+    
+    //test print method
+    public void PrintMethod(){
+        System.out.println("Print method");
+        //Test commit 11:02
+    }
+    
+}
